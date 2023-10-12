0ba34102c71e403a0cb4fcae497363978aab9632
Depika Gupta <depikag21@gmail.com>
2023-10-12 21:15:39 +0530

    introducing voice command feature

--- main.py
+++ main.py
@@ -2,9 +2,11 @@ import os
 import cv2
 from cvzone.HandTrackingModule import HandDetector
 import numpy as np
+import speech_recognition as sr
 
 # Variables
 width, height = 1280, 720
+gestureThreshold = 300
 folderPath = "Presentation"
 
 # Camera Setup
@@ -17,18 +19,27 @@ pathImages = sorted(os.listdir(folderPath), key=len)
 # print(pathImages)
 
 # Variables
-imgNumber = 4
-hs, ws = int(120*1), int(213*1) # width and height of small image
-gestureThreshold = 300
+imgList = []
+delay = 30
 buttonPressed = False
-buttonCounter =0
-buttonDelay = 30
+counter = 0
+drawMode = False
+imgNumber = 0
+delayCounter = 0
 annotations = [[]]
 annotationNumber = 0
 annotationStart = False
+hs, ws = int(120 * 1), int(213 * 1)  # width and height of small image
 
 # Hand Detector
-detector = HandDetector(detectionCon=0.8, maxHands=1)
+detector = HandDetector(detectionCon=0.8, maxHands=2)
+
+# Speech Recognizer
+recognizer = sr.Recognizer()
+
+# Zoom Parameters
+zoomScale = 1.0
+zoomSpeed = 0.02
 
 while True:
     # Import Images
@@ -47,15 +58,14 @@ while True:
         lmList = hand['lmList']
 
         # Constrain values for easier drawing
-        xVal = int(np.interp(lmList[8][0], [width//2, w], [0, width]))
+        xVal = int(np.interp(lmList[8][0], [width//2, width], [0, width]))
         yVal = int(np.interp(lmList[8][1], [150, height-150], [0, height]))
         indexFinger = xVal, yVal
-
 
         if cy <= gestureThreshold: # if hand is at the height of the face
             annotationStart = False
             # Gesture 1 - Left
-            if fingers == [1,0,0,0,0]:
+            if fingers == [1, 0, 0, 0, 0]:
                 annotationStart = False
                 print ("Left")
                 if imgNumber > 0:
@@ -97,14 +107,63 @@ while True:
                     annotations.pop(-1)
                     annotationNumber -= 1
                     buttonPressed = True
+
+        # Gesture 6 - Zoom in/out
+        if fingers == [0, 1, 1, 1, 1]:
+            zoomScale += zoomSpeed
+            print("Zoom In:", zoomScale)
+        if fingers == [1, 1, 1, 1, 1]:
+            zoomScale -= zoomSpeed
+            if zoomScale < 1.0:
+                zoomScale = 1.0
+            print("Zoom Out:", zoomScale)
+
+        # Gesture 7 - "Go to Slide No. __ "
+        if cy > gestureThreshold:
+            if fingers == [1, 0, 0, 0, 0]:
+                buttonPressed = True
+                print("Go to specific slide gesture detected")
+                try:
+                    # Prompt user for slide number using voice command
+                    with sr.Microphone() as source:
+                        print("Listening for slide number...")
+                        recognizer.pause_threshold = 1
+                        audio = recognizer.listen(source)
+                        # Recognize the voice command
+                        command = recognizer.recognize_google(audio, language="en-in").lower()
+                        print(f"Voice Command: {command}")
+                        # Extract slide number from command
+                        slide_number = int(command.split("slide")[-1].strip())
+                        # Verify and navigate to the desired slide
+                        if 0 < slide_number <= len(pathImages):
+                            imgNumber = slide_number -1
+                            annotations = [[]]
+                            annotationNumber = -1
+                            annotationStart = False
+                            print(f"Going to slide {slide_number}")
+                            for path in pathImages:
+                                if (path == imgNumber):
+                                    cv2.imshow("Image", img)
+
+                        else:
+                            print("Invalid slide number")
+                            exit()
+
+                except sr.UnknownValueError:
+                    print("Could not understand audio")
+
+                except sr.RequestError as e:
+                    print(f"Could not request results from Google Speech Recognition service; {e}")
+                    exit()
+
     else:
         annotationStart = False
 
     # Button Pressed Iterations
     if buttonPressed:
-        buttonCounter += 1
-        if buttonCounter > buttonDelay:
-            buttonCounter = 0
+        counter += 1
+        if counter > delay:
+            counter = 0
             buttonPressed = False
 
     for i in range(len(annotations)):
@@ -112,11 +171,12 @@ while True:
             if j != 0:
                 cv2.line(imgCurrent, annotations[i][j-1], annotations[i][j], (0, 0, 200), 12)
 
+    imgCurrent = cv2.resize(imgCurrent, None, fx=zoomScale, fy=zoomScale)
+
     # Adding Webcam image on the slides
     imgSmall = cv2.resize(img, (ws, hs))
     h, w, _ = imgCurrent.shape
     imgCurrent[0:hs,w-ws:w] = imgSmall
-
 
     cv2.imshow("Image", img)
     cv2.imshow("Slides", imgCurrent)
@@ -124,3 +184,5 @@ while True:
     key = cv2.waitKey(1)
     if key == ord('q'):
         break
+
+cv2.destroyAllWindows()
